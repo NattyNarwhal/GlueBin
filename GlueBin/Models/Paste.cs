ec3117b2206b4d6c357db6dbdfa4597ee5b8ec70
Calvin <calvin@openmailbox.org>
2017-07-20 12:22:46 -0300

    Document models

--- GlueBin/Models/Paste.cs
+++ GlueBin/Models/Paste.cs
@@ -6,19 +6,52 @@ using System.Web;
 
 namespace GlueBin.Models
 {
+    /// <summary>
+    /// Represents a paste and its metadata.
+    /// </summary>
     public class Paste
     {
+        /// <summary>
+        /// The MongoDB object ID. This must be unique.
+        /// </summary>
         public ObjectId _id;
 
+        /// <summary>
+        /// The name of the paste. This must be unique.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The contents of the paste.
+        /// </summary>
         public string Content { get; set; }
+        /// <summary>
+        /// The username of the user who posted this paste.
+        /// </summary>
         public string UserName { get; set; }
+        /// <summary>
+        /// The syntax highlighting language that the paste uses.
+        /// </summary>
+        /// <remarks>
+        /// This must be a name that <see cref="Highlight.Highlighter"/>
+        /// understands.
+        /// </remarks>
         public string HighlightLanguage { get; set; }
+        /// <summary>
+        /// The time the paste was updated.
+        /// </summary>
         public DateTime Posted { get; set; }
+        /// <summary>
+        /// If the paste should be shown publically in listings.
+        /// </summary>
         public bool Public { get; set; } 
 
-        // always false at storage time, this is only set at runtime
-        // for rendering
+        /// <summary>
+        /// If the content should be escaped when rendered or not.
+        /// </summary>
+        /// <remarks>
+        /// This is always false when stored - that is, the variable is only
+        /// set during rendering.
+        /// </remarks>
         public bool RenderAsHtml { get; set; }
     }
 }
--- GlueBin/Models/UserIdentity.cs
+++ GlueBin/Models/UserIdentity.cs
@@ -7,14 +7,39 @@ using System.Web;
 
 namespace GlueBin.Models
 {
+    /// <summary>
+    /// Represents a user; which is then passed to Nancy as well as stored in
+    /// the database.
+    /// </summary>
     public class UserIdentity : IUserIdentity
     {
+        /// <summary>
+        /// The MongoDB object ID. This must be unique.
+        /// </summary>
         public ObjectId _id;
 
-        // Claims: Paste
+        /// <summary>
+        /// The rights a user has.
+        /// </summary>
+        /// <remarks>
+        /// Possible rights:
+        /// <list type="">
+        /// <item>Paste</item>
+        /// <item>DeleteAny</item>
+        /// </list>
+        /// </remarks>
         public IEnumerable<string> Claims { get; set; }
+        /// <summary>
+        /// The user's name.
+        /// </summary>
         public string UserName { get; set; }
+        /// <summary>
+        /// The password after being combined with the salt and hashed.
+        /// </summary>
         public string HashedPassword { get; set; }
+        /// <summary>
+        /// The salt, which is combined with the password before hashing.
+        /// </summary>
         public string Salt { get; set; }
     }
 }
